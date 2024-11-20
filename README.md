ebc5157abe1b82126e16e5f9f91a758be0e24935
Maxim <maximgratii@gmail.com>
2024-11-20 18:36:08 +0200

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# @upvoty/next
+
+The [Upvoty](https://www.upvoty.com/) widget for your Next.js app.
+
+## Usage
+
+See [the documentation](https://app.upvoty.com/instructions)
+
+## License
+
+Licensed under the MIT license.
---
+++ index.tsx
@@ -0,0 +1,24 @@
+import Script from "next/script";
+import * as React from "react";
+
+interface UpvotyProps {
+  projectId: string;
+  lang?: string;
+}
+
+const Upvoty: React.FC<UpvotyProps> = ({ projectId, lang }) => {
+  if (!projectId) {
+    console.error("No projectId was passed to Upvoty Next, widget will not be initialized");
+    return null;
+  }
+
+  return (
+    <Script
+      src={`https://app.upvoty.com/embed.js?id=${projectId}`}
+      strategy="afterInteractive"
+      lang={lang}
+    />
+  );
+};
+
+export default Upvoty;
